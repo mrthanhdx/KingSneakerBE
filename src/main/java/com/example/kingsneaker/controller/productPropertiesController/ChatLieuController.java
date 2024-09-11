5b845da88347c16d2129c216c5ac37e16c0d862b
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-09-11 11:32:25 +0700

    update

---
+++ src/main/java/com/example/kingsneaker/controller/productPropertiesController/ChatLieuController.java
@@ -0,0 +1,9 @@
+package com.example.kingsneaker.controller.productPropertiesController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/chat-lieu")
+public class ChatLieuController {
+}
