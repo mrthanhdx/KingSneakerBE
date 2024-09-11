0ef573b9d4227d363ba81e9e48f392306c5de7e2
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-09-11 09:38:29 +0700

    update

--- src/main/java/com/example/kingsneaker/controller/productPropertiesController/KichCoController.java
+++ src/main/java/com/example/kingsneaker/controller/productPropertiesController/KichCoController.java
@@ -1,4 +1,8 @@
 package com.example.kingsneaker.controller.productPropertiesController;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class KichCoController {
 }
