13da230e909c6be081c66acff4698e13d51fdbe1
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-09-11 11:26:17 +0700

    update

--- src/main/java/com/example/kingsneaker/controller/productPropertiesController/KichCoController.java
+++ src/main/java/com/example/kingsneaker/controller/productPropertiesController/KichCoController.java
@@ -1,9 +1,15 @@
 package com.example.kingsneaker.controller.productPropertiesController;
 
+import com.example.kingsneaker.entity.KichCo;
 import com.example.kingsneaker.service.KichCoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/kich-co")
@@ -11,4 +17,9 @@ public class KichCoController {
 
     @Autowired
     KichCoService kichCoService;
+
+    @GetMapping("/show-all")
+    public ResponseEntity<List<KichCo>> showAllKichCo(){
+        return new ResponseEntity<>(kichCoService.findAll(), HttpStatus.OK);
+    }
 }
