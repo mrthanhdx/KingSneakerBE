9910ddf8222a5e2a55681441c45580a0896ba0d0
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-09-11 11:38:09 +0700

    update

--- src/main/java/com/example/kingsneaker/controller/productPropertiesController/ChatLieuController.java
+++ src/main/java/com/example/kingsneaker/controller/productPropertiesController/ChatLieuController.java
@@ -1,9 +1,13 @@
 package com.example.kingsneaker.controller.productPropertiesController;
 
+import com.example.kingsneaker.service.ChatLieuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/chat-lieu")
 public class ChatLieuController {
+    @Autowired
+    ChatLieuService chatLieuService;
 }
