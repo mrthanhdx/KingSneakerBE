6c3a87c0789bf6e6ea68d5048afb49bc4cdea50c
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-09-11 11:22:44 +0700

    update

--- src/main/java/com/example/kingsneaker/controller/productPropertiesController/KichCoController.java
+++ src/main/java/com/example/kingsneaker/controller/productPropertiesController/KichCoController.java
@@ -1,8 +1,14 @@
 package com.example.kingsneaker.controller.productPropertiesController;
 
+import com.example.kingsneaker.service.KichCoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/api/v1/kich-co")
 public class KichCoController {
+
+    @Autowired
+    KichCoService kichCoService;
 }
