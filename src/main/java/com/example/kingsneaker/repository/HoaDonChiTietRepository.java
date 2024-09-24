bfcd499ac0f33422afcf8c45cceef58048dd9400
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-09-24 16:51:10 +0700

    update

--- src/main/java/com/example/kingsneaker/repository/HoaDonChiTietRepository.java
+++ src/main/java/com/example/kingsneaker/repository/HoaDonChiTietRepository.java
@@ -2,8 +2,10 @@ package com.example.kingsneaker.repository;
 
 import com.example.kingsneaker.entity.HoaDonChiTiet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,4 +13,18 @@ import java.util.List;
 public interface HoaDonChiTietRepository extends JpaRepository<HoaDonChiTiet,Long> {
     @Query(value = "select * from hoa_don_chi_tiet where id_hoa_don = ?1",nativeQuery = true)
     List<HoaDonChiTiet> getHoaDonChiTietsByIdHd(Long idHD);
+
+    @Query(value = "select * from hoa_don_chi_tiet where id_hoa_don = ?1 and id_chi_tiet_sp = ?2",nativeQuery = true)
+    HoaDonChiTiet getHoaDonChiTietsByIdHdAndIdCtsp(Long idHD,Long idCTSP);
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete from hoa_don_chi_tiet where id_hoa_don= ?1 and id_chi_tiet_sp = ?2",nativeQuery = true)
+    void deleteHDCTByIdHdAndIdSpct(Long idHoaDon,Long idSPCT);
+
+
+    @Query(value = "update hoa_don_chi_tiet set so_luong = ?1 where id_chi_tiet_sp = ?2 and id_hoa_don = ?3",nativeQuery = true)
+    void updateSoLuongByIdCtspAndidHd(Integer soLuong,Long idCTSP,Long idHD);
+
+
 }
