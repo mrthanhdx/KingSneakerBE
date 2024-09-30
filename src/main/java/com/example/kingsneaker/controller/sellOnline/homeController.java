b348c9bcfd64e73b88cd1ca6fa3f5a89af63855e
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-09-30 22:03:44 +0700

    update

---
+++ src/main/java/com/example/kingsneaker/controller/sellOnline/homeController.java
@@ -0,0 +1,9 @@
+package com.example.kingsneaker.controller.sellOnline;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/home")
+public class homeController {
+}
